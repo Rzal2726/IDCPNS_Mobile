a1edd98476a4a5d82b1ede15c6db2610487ada5c
Rzal <138451850+Rzal2726@users.noreply.github.com>
2025-08-25 15:49:32 +0700

    tryout

---
+++ lib/app/modules/tryout/bindings/tryout_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:idcpns_mobile/app/data/rest_client_provider.dart';
+
+import '../controllers/tryout_controller.dart';
+
+class TryoutBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<TryoutController>(() => TryoutController());
+    Get.lazyPut<RestClientProvider>(() => RestClientProvider());
+  }
+}
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,19 +2,26 @@ import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/tryout/bindings/tryout_binding.dart';
+import '../modules/tryout/views/tryout_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.TRYOUT;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.TRYOUT,
+      page: () => TryoutView(),
+      binding: TryoutBinding(),
     ),
   ];
 }
